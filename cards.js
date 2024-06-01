17ae95c54a98552f256e077a1fd45779fe319d46
Yeremi74 <yyeremi15@gmail.com>
2024-06-01 14:15:53 -0400

    adding cards info

---
+++ cards.js
@@ -0,0 +1,14 @@
+[
+  {
+    title: 'Abstract Away K8s Primitives',
+    desc: 'Deploy, manage and debug applications with end-to-end ownership without experiencing bottlenecks or battling Kubernetes hurdles.',
+  },
+  {
+    title: 'Proactively Troubleshoot',
+    desc: 'Identify availability issues, misconfigurations, lack of resources, K8s anomalies and failed deploys w. feedback and health checks.',
+  },
+  {
+    title: 'Developer Tools',
+    desc: 'Robust tooling that encompass a CLI, an extensive developer-interfaces and cloud-native CI/CD integrations.',
+  },
+];
