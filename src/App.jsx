cc6cc06d1ad403acfff9fbf4e5af513ce1fb8639
Yeremi74 <yyeremi15@gmail.com>
2024-06-08 18:11:10 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,10 @@ import Cards from './components/Cards';
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
       <Benefit />
       <Cards />
     </>
   );
-=======
-    <Benefit />
-    <Cards />
-    </>
-  )
->>>>>>> aaf9a4330bcb3e8d9d534cd3efd77c311bd00cc8
 };
 
 export default App;
